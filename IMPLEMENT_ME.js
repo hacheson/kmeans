8c2eb8d0edfd120b828d159dfa283502bf33df29
hannah <hachesonfield@gmail.com>
2014-01-09 12:19:41 -0800

    first commit

--- IMPLEMENT_ME.js
+++ IMPLEMENT_ME.js
@@ -36,6 +36,23 @@ var points = [];
 
 /* For each point, assign it to the cluster represented by the closest centroid */
 function assignCentroids() {
+
+  for(var p = 0; p < points.length; p++){
+    var curr_best = Number.MAX_VALUE;
+    var curr_centroid;
+    var point = points[p];
+    for(var c = 0; c < centroids.length; c++){
+      var centroid = centroids[c];
+
+      var dist = Math.sqrt(((point.x - centroid.x)*(point.x - centroid.x)) +((point.y - centroid.y)*(point.y - centroid.y)));
+      if (dist < curr_best){
+        curr_best = dist;
+        curr_centroid = centroid;
+      }
+    }
+    point.centroid = curr_centroid;
+  }
+
   
 }
 
@@ -43,5 +60,25 @@ function assignCentroids() {
   The new position should be the mean of the positions of the points assigned to it.
 */
 function updateCentroids() {
-  
+  for(var c = 0; c < centroids.length; c++){
+    var centroid = centroids[c];
+    var x_sum = 0;
+    var y_sum = 0;
+    var p_count = 0;
+    for(var p = 0; p < points.length; p++){
+
+      var point= points[p];
+      if(point.centroid == centroid){
+        p_count ++;
+        x_sum += point.x;
+        y_sum += point.y;
+      }
+      
+    }
+    centroid.x = x_sum/p_count;
+    centroid.y = y_sum/p_count;
+  }
 }
+
+
+
